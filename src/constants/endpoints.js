1e80d85a7d9f1593ba4650bab8d7e6a5284f903f
prannoy <prannoy.mulmi@gmail.com>
2022-09-14 09:03:56 +0200

    added relative path for home endpoint

--- src/constants/endpoints.js
+++ src/constants/endpoints.js
@@ -1,7 +1,7 @@
 const endpoints = {
   navbar: 'profile/navbar.json',
   routes: 'profile/routes.json',
-  home: 'profile/home.json',
+  home: './profile/home.json',
   social: 'profile/social.json',
   about: 'profile/about.json',
   skills: 'profile/skills.json',
