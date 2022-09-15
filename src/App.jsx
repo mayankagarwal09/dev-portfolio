e8b2473b6373725ae9f38dd55b1ce4810fb0ed28
prannoy <prannoy.mulmi@gmail.com>
2022-09-15 09:00:24 +0200

    Using syslink

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import useDarkMode from 'use-dark-mode';
 import AppContext from './AppContext';
@@ -17,9 +17,9 @@ function App() {
       <ThemeProvider theme={darkMode.value ? darkTheme : lightTheme}>
         <GlobalStyles />
         <div className="App">
-          <HashRouter basename="/e-portfolio">
+          <BrowserRouter basename="/e-portfolio">
             <MainApp />
-          </HashRouter>
+          </BrowserRouter>
         </div>
       </ThemeProvider>
     </AppContext.Provider>
