e874d6858a9f7c028ea50960fe2209a779446bad
prannoy <prannoy.mulmi@gmail.com>
2022-09-30 22:13:39 +0200

    reverted basename

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
       <ThemeProvider theme={darkMode.value ? darkTheme : lightTheme}>
         <GlobalStyles />
         <div className="App">
-          <BrowserRouter basename="/e-portfolio">
+          <BrowserRouter basename={BASE_NAME}>
             <MainApp />
           </BrowserRouter>
         </div>
