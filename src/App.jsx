2ccfe9a91e78661ca3531a9994bdf09f8622eab7
Selaka Nanayakkara <selaka.nanayakkara@cube.global>
2022-10-30 18:53:38 +0530

    added firebase configs

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,23 @@ import AppContext from './AppContext';
 import MainApp from './MainApp';
 import GlobalStyles from './theme/GlobalStyles';
 import { lightTheme, darkTheme } from './theme/themes';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDuZBtHAhq9RohACFjzcreux7L1JJBnOV0",
+  authDomain: "selaka-cv.firebaseapp.com",
+  projectId: "selaka-cv",
+  storageBucket: "selaka-cv.appspot.com",
+  messagingSenderId: "232715702190",
+  appId: "1:232715702190:web:baded6bb154a36ee70c23f",
+  measurementId: "G-76Q6V7VZQ5"
+};
+
 
 function App() {
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
   window.matchMedia = null;
   const darkMode = useDarkMode(true);
 
